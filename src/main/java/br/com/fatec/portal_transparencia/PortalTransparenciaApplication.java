ff898db4f762f2ba0f17613430c715246b31a83b
Victor-Creator-Ops <victor.feitosa@fatec.sp.gov.br>
2026-05-23 17:01:00 -0300

    commit inicial: estrutura spring boot e configuracao do mysql

---
+++ src/main/java/br/com/fatec/portal_transparencia/PortalTransparenciaApplication.java
@@ -0,0 +1,13 @@
+package br.com.fatec.portal_transparencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalTransparenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalTransparenciaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fatec/portal_transparencia/PortalTransparenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fatec.portal_transparencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalTransparenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
